60854fef1b7ca97690c3183a93e15ee3511e4f01
tuliobarreto <tuliopaesbarreto@gmail.com>
2019-07-20 18:18:49 -0300

    2 files modified

--- index.js
+++ index.js
@@ -5,3 +5,48 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 */
+
+
+
+var canvas = document.getElementById("mycanvas");
+var ctx = canvas.getContext("2d");
+
+var tecla = {};
+
+var bola = {
+  x: canvas.width / 2 - 15,
+  y: canvas.height / 2 - 15,
+  altura: 30,
+  largura: 30,
+  dirx: -1,
+  diry: 1,
+  mod: 0,
+  speed: 50
+};
+
+var direita = {
+  x: 10,
+  y: canvas.height / 2 - 60,
+  altura: 120,
+  largura: 30,
+  score: 0,
+  speed: 15
+};
+
+var esquerda ={
+  x: 560,
+  y: canvas.height / 2 - 60,
+  altura: 120,
+  largura: 30,
+  score: 0,
+  speed: 15
+};
+
+function desenha(){
+  ctx.fillStyle = "white";
+  ctx.fillRect(bola.x, bola.y, bola.largura, bola.altura);
+  ctx.fillRect(direita.x, direita.y, direita.largura, direita.altura);
+  ctx.fillRect(esquerda.x, esquerda.y, esquerda.largura, esquerda.altura);
+}
+
+desenha();
